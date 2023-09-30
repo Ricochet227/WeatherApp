ff966c94beea5b9128f565535176a58df266b3b3
Ricochet227 <skyler7crawford@gmail.com>
2023-09-30 02:47:45 -0600

    Fixed LocalStorage
    I fixed the local storage to save and display contents even on refresh

--- script.js
+++ script.js
@@ -1,95 +1,116 @@
-var previousSearches = [];
 const cityInput = document.querySelector(".city-input");
 const searchBtn = document.querySelector(".search-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
-
 const API_KEY = "002451757e9cc5352b12c6259bc48026";
+let previousSearches = [];
 
-const createWeatherCard = (cityName, weatherItem, index) => {
-    if(index === 0){
-        return `<div class="details">
-                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-                    <h4>Temperature: ${((weatherItem.main.temp - 273.15) * 9/5 + 32).toFixed(2)}°F</h4>
-                    <h4>Wind: ${weatherItem.wind.speed}mph</h4>
-                    <h4>Humidity: ${weatherItem.main.humidity}%</h4>
-                </div>
-                <div class="icon">
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                    <h4>${weatherItem.weather[0].description.toUpperCase()}</h4>
-                </div>`
-    }else{
-    return`<li class="card">
-            <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
-            <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
-            <h4>Temp: ${((weatherItem.main.temp - 273.15) * 9/5 + 32).toFixed(2)}°F</h4>
-            <h4>Wind: ${weatherItem.wind.speed}mph</h4>
-            <h4>Humidity: ${weatherItem.main.humidity}%</h4>
-        </li>`
-    }
-
-}
-const storedSearches = localStorage.getItem('previousSearches');
-
-function renderPreviousSearches() {
-    const storedSearches = localStorage.getItem('previousSearches');
-    if (storedSearches) {
-        const previousSearches = JSON.parse(storedSearches);
-      }
-    var previousSearchesEl = document.getElementById("previous-searches");
-    localStorage.setItem('previousSearches', JSON.stringify(previousSearches));
-    previousSearchesEl.innerHTML = "";
-    for (var i = 0; i < previousSearches.length; i++) {
-      var listItemEl = document.createElement("li");
-      listItemEl.textContent = previousSearches[i];
-      previousSearchesEl.appendChild(listItemEl);
+function createWeatherCard(cityName, weatherItem, index) {
+    if (index === 0) {
+      return `
+        <div class="details">
+          <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+          <h4>Temperature: ${((weatherItem.main.temp - 273.15) * 9/5 + 32).toFixed(2)}°F</h4>
+          <h4>Wind: ${weatherItem.wind.speed}mph</h4>
+          <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+        </div>
+        <div class="icon">
+          <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon">
+          <h4>${weatherItem.weather[0].description.toUpperCase()}</h4>
+        </div>
+      `;
+    } else {
+      return `
+        <li class="card">
+          <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+          <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon">
+          <h4>Temp: ${((weatherItem.main.temp - 273.15) * 9/5 + 32).toFixed(2)}°F</h4>
+          <h4>Wind: ${weatherItem.wind.speed}mph</h4>
+          <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+        </li>
+      `;
     }
   }
 
-const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+  function renderPreviousSearches() {
+    const previousSearchesEl = document.getElementById("previous-searches");
+    previousSearchesEl.innerHTML = "";
+    previousSearches.forEach(search => {
+      const listItemEl = document.createElement("li");
+      listItemEl.textContent = search;
+      previousSearchesEl.appendChild(listItemEl);
+    });
+  }
+  
+  function getWeatherDetails(cityName, lat, lon) {
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
     previousSearches.push(cityName);
     renderPreviousSearches();
-    console.log(previousSearches)
-    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
-        const uniqueForecastDays = []
-        const fiveDaysForecast = data.list.filter(forecast =>{
-            const forecastDate = new Date(forecast.dt_txt).getDate()
-            if(!uniqueForecastDays.includes(forecastDate)) {
-                return uniqueForecastDays.push(forecastDate);
-            }
+    fetch(WEATHER_API_URL)
+      .then(res => res.json())
+      .then(data => {
+        currentWeatherDiv.innerHTML = "";
+        weatherCardsDiv.innerHTML = "";
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+          const forecastDate = new Date(forecast.dt_txt).getDate();
+          if (!uniqueForecastDays.includes(forecastDate)) {
+            uniqueForecastDays.push(forecastDate);
+            return true;
+          }
+          return false;
         });
-
-        cityInput.value = "";
-        currentWeatherDiv.innerHTML= "";
-        weatherCardsDiv.innerHTML= "";
-
-        console.log(fiveDaysForecast)
-        fiveDaysForecast.forEach((weatherItem, index)=> {
-            if(index === 0) {
-                currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index))
-            }else{
-                weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index))
-            }
-        })
-    }).catch(() => {
-        alert("An error occurred while fethcing the weather forecast!")
+        fiveDaysForecast.forEach((weatherItem, index) => {
+          if (index === 0) {
+            currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+          } else {
+            weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+          }
+        });
       })
-}
-
-const getCityCoordinates = () => {
-  const cityName = cityInput.value.trim();
-  if (!cityName) return;
-  const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
-
-  fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-    if(!data.length) return alert(`No coordinates found for ${cityName}`)
-    const{ name, lat, lon } = data[0];
-    getWeatherDetails(name, lat, lon);
-  }).catch(() => {
-    alert("An error occurred while fethcing the coordinates!")
-  })
-}
-
-searchBtn.addEventListener("click", getCityCoordinates);
-cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates())
+      .catch(() => {
+        alert("An error occurred while fetching the weather forecast!");
+      });
+  }
+  
+  function getCityCoordinates() {
+    const cityName = cityInput.value.trim();
+    if (!cityName) return;
+  
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    fetch(GEOCODING_API_URL)
+      .then(res => res.json())
+      .then(data => {
+        if (!data.length) {
+          return alert(`No coordinates found for ${cityName}`);
+        }
+  
+        const { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon);
+        savePreviousSearches();
+      })
+      .catch(() => {
+        alert("An error occurred while fetching the coordinates!");
+      });
+  }
+  
+  function savePreviousSearches() {
+    localStorage.setItem("previousSearches", JSON.stringify(previousSearches));
+  }
+  
+  function loadPreviousSearches() {
+    const storedSearches = localStorage.getItem("previousSearches");
+    if (storedSearches) {
+      previousSearches = JSON.parse(storedSearches);
+      renderPreviousSearches();
+    }
+  }
+  
+  searchBtn.addEventListener("click", getCityCoordinates);
+  cityInput.addEventListener("keyup", e => {
+    if (e.key === "Enter") {
+      getCityCoordinates();
+    }
+  });
+  
+  loadPreviousSearches();
