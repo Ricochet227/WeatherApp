3800efd9848e0722216a6d779bb17f5a1a3eb46c
Ricochet227 <skyler7crawford@gmail.com>
2023-09-30 03:05:30 -0600

    Deleted unneeded files
    Removed files that are no longer needed

--- alt.js
+++
@@ -1,8 +0,0 @@
-const searchResultButtons = document.querySelectorAll('.search-result-button');
-
-searchResultButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    // Perform desired action when a search result button is clicked
-    console.log("Search result button clicked for city:", button.textContent);
-  });
-});
