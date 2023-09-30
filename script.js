efe3a1a725b74087bfe12d1706b48998ec811414
Ricochet227 <skyler7crawford@gmail.com>
2023-09-30 03:49:35 -0600

    Fixed the date format
    Fixed how the date was formatted incorrectly

--- script.js
+++ script.js
@@ -4,32 +4,39 @@ const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 const API_KEY = "002451757e9cc5352b12c6259bc48026";
 let previousSearches = [];
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return date.toLocaleDateString(undefined, options);
+}
+
 function createWeatherCard(cityName, weatherItem, index) {
-    if (index === 0) {
-      return `
-        <div class="details">
-          <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-          <h4>Temperature: ${((weatherItem.main.temp - 273.15) * 9/5 + 32).toFixed(2)}°F</h4>
-          <h4>Wind: ${weatherItem.wind.speed}mph</h4>
-          <h4>Humidity: ${weatherItem.main.humidity}%</h4>
-        </div>
-        <div class="icon">
-          <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon">
-          <h4>${weatherItem.weather[0].description.toUpperCase()}</h4>
-        </div>
-      `;
-    } else {
-      return `
-        <li class="card">
-          <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
-          <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon">
-          <h4>Temp: ${((weatherItem.main.temp - 273.15) * 9/5 + 32).toFixed(2)}°F</h4>
-          <h4>Wind: ${weatherItem.wind.speed}mph</h4>
-          <h4>Humidity: ${weatherItem.main.humidity}%</h4>
-        </li>
-      `;
-    }
+  if (index === 0) {
+    return `
+      <div class="details">
+        <h2>${cityName} (${formatDate(weatherItem.dt_txt)})</h2>
+        <h4>Temperature: ${((weatherItem.main.temp - 273.15) * 9/5 + 32).toFixed(2)}°F</h4>
+        <h4>Wind: ${weatherItem.wind.speed}mph</h4>
+        <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+      </div>
+      <div class="icon">
+        <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon">
+        <h4>${weatherItem.weather[0].description.toUpperCase()}</h4>
+      </div>
+    `;
+  } else {
+    return `
+      <li class="card">
+        <h3>(${formatDate(weatherItem.dt_txt)})</h3>
+        <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon">
+        <h4>Temp: ${((weatherItem.main.temp - 273.15) * 9/5 + 32).toFixed(2)}°F</h4>
+        <h4>Wind: ${weatherItem.wind.speed}mph</h4>
+        <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+      </li>
+    `;
   }
+}
 
   function renderPreviousSearches() {
     const previousSearchesEl = document.getElementById("previous-searches");
